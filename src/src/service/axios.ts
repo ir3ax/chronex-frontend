606128c02b2949f9e8bf73341a4d41c25045704c
Mark Limuel <it.marklimuelfernando@gmail.com>
2024-05-01 11:12:41 +0800

    change-api-url-for-deployment

--- src/src/service/axios.ts
+++ src/src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://chronex-backend.onrender.com/'
-const API_URL = 'http://localhost:8000/'
+const API_URL = 'https://chronex-backend.onrender.com/'
+// const API_URL = 'http://localhost:8000/'
 export const axiosInstance = axios.create({
 	baseURL: API_URL,
 	headers: {
